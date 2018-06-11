63c1d5e64088e250b3dd85f9e2290d644ee1f2c4
Chris <35409541+kristoDev@users.noreply.github.com>
2018-06-11 11:03:44 +0200

    updated
    done

--- TripeCost_calculator.py
+++ TripeCost_calculator.py
@@ -8,10 +8,26 @@ b23_trvl = traveled km
 '''
 
 
-b21_ptCost = 14
-b22_lpkm = 10
-b23_trvl = 30
+from decimal import Decimal
+
+
+b0 = input('Enter (in numbers) the gaz cost per liter:')
+b21_ptCost = int(b0)
+
+currency = input('Enter your currency sign:')
+
+b01 = input('Enter (in numbers) the car fuel consumption (100km/l):')
+b22_lpkm = int(b01)
+
+b02 = input('Enter (in numbers) the the km distance you want to travel:')
+b23_trvl = int(b02) 
+
 
 tripeCost = (b23_trvl/(100/b22_lpkm))*b21_ptCost
+convFloat_to_Dc = Decimal(tripeCost)
+lim_to_2 = round(convFloat_to_Dc,2)
 
-print (tripeCost)
+strConv = str(lim_to_2)
+
+
+print ('**** '+'This trip will cost you '+currency+' '+ strConv+' ****')
